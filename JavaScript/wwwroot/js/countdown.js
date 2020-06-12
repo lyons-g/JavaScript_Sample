67a29e73a7158f04d113b951ca8d6be10d3d3531
lyons-g <62383079+lyons-g@users.noreply.github.com>
2020-06-12 16:14:50 +0100

    Add project files.

---
+++ JavaScript/wwwroot/js/countdown.js
@@ -0,0 +1,89 @@
+﻿const months = [
+    "Jan", "Feb", "Mar", "April", "May", "June", "July", "August", "Sept", "Oct", "Nove", "Dec"
+];
+
+const weekdays = [ 
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+//let futureDate = new Date(2020,7,30,17,30,0);
+//console.log(futureDate);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}pm`;
+
+
+//future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+    const today = new Date().getTime();
+    const t = futureTime - today;
+   // console.log(t);
+    //1s = 1000ms
+    //1m = 60s
+    //1hr = 60min
+    //1d = 24hr
+
+    //values in ms
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    //calculate
+    let days = t / oneDay;
+    days = Math.floor(days);
+
+    let hours = Math.floor((t % oneDay) / oneHour);
+
+   // console.log(hours);
+
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+  //  console.log(minutes);
+
+    let seconds = Math.floor((t % oneMinute) / 1000);
+  //  console.log(seconds);
+
+    //set values array
+    const values = [days, hours, minutes, seconds];
+
+    function format(item) {
+        if (item < 10) {
+            return item =`0${item}`
+        }
+        return item
+    }
+
+    items.forEach(function (item, index) {
+        item.innerHTML = format(values[index]);
+    });
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired"> Sorry, this giveaway has expired<h1>`;
+    }
+   
+}
+
+//countdown
+let countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
---
+++ JavaScript/wwwroot/js/site.js
@@ -0,0 +1,66 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function myFunction() {
+    //document.getElementById("demo").innerHTML = "Paragraph changed";
+
+    //document.write for testing purpose only, deletes all other HTML
+    //  document.write(5 + 6);
+
+    //window.
+    alert("myFunction called");
+
+    var fruits = ["Banana", "Apple", "Orange"];
+    // var fLen = fruits.length;
+    //var i;
+    var text;
+
+    text = "<ul>";
+    fruits.forEach(forEachFunction);
+
+    // for (i = 0; i < fLen; i++) {
+    //   text += "<li>" + fruits[i] + "</li>";
+    // }
+    text += "</ul>";
+    document.getElementById("arrayDemo").innerHTML = text;
+
+    function forEachFunction(value) {
+        text += "<li>" + value + "</li>";
+    }
+}
+
+function ageFunction() {
+    var age, voteable;
+    age = Number(document.getElementById("age").value);
+    if (isNaN(age)) {
+        voteable = "Input is not a number";
+    }
+    else {
+        voteable = (age < 18) ? "Too young" : "Old enough";
+    }
+    document.getElementById("ageDemo").innerHTML = voteable;
+}
+
+/*
+ * ColorFlipper
+ */
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function () {
+   // console.log(document.body);
+    //get random number between 0-3 
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+
+}
